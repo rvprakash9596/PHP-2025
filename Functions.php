7d0f4bf63119d70de0c0f4a2c3b822a93fa5bb81
rvprakash9596 <raviprakash9596@gmail.com>
2025-02-20 20:43:35 +0530

    Function In  PHP

--- Functions.php
+++ Functions.php
@@ -1,6 +1,44 @@
 ﻿<?php
 echo"<h1><center>Functions in PHP</center></h1>";
-echo"A function is a block of statement that can be used repeatedly  in a program.<br> A function will not execute automatically when a page loads.<br> A function will be executed by a call to the function.<br> A function may have zero , one or more parameters.<br> A function may return a value.";
+echo"A function is a block of statement that can be used repeatedly  in a program.<br> A function will not execute automatically when a page loads.<br> A function will be executed by a call to the function.<br> A function may have zero , one or more parameters.<br> A function may echo a value.<br> 1.Pre Defined Functions : Defined at the time of making any Language. <br> 
+2. User Defined Functions : Defined by users according to their needs.<br><br><br>";
+
+//1. User Defined Functions
+
+/*
+function greet(){
+echo"Hello , Welcome to you !";
+}
+greet();
+*/
+
+
+/*
+function greet(){
+    return"Hello , Bhai !";
+}
+   echo greet();
+*/
+
+
+/*
+function familyName($fname){
+    echo"$fname.<br>";
+}
+familyName("Ravi");
+familyName("Sonu");
+familyName("Vidya");
+familyName("Kumkum");
+*/
+
+/*
+function familyName($fname,$age){
+    echo"$fname and $age .<br>";
+}
+familyName("Ravi",23);
+familyName("Ravi",23);
+familyName("Ravi",23);
+*/
 
 
 
