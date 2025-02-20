12d9ff7d5b1849e25e6ef2e06a4951db7dec66f9
rvprakash9596 <raviprakash9596@gmail.com>
2025-02-20 20:44:48 +0530

    Functions

--- Functions.php
+++ Functions.php
@@ -1,4 +1,5 @@
 ﻿<?php
+
 echo"<h1><center>Functions in PHP</center></h1>";
 echo"A function is a block of statement that can be used repeatedly  in a program.<br> A function will not execute automatically when a page loads.<br> A function will be executed by a call to the function.<br> A function may have zero , one or more parameters.<br> A function may echo a value.<br> 1.Pre Defined Functions : Defined at the time of making any Language. <br> 
 2. User Defined Functions : Defined by users according to their needs.<br><br><br>";
