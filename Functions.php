2e5440acd7d34bee16467e86871b24b462cf8c58
rvprakash9596 <raviprakash9596@gmail.com>
2025-02-20 11:17:04 +0530

    Functions in PHP

--- Functions.php
+++ Functions.php
@@ -1,4 +1,8 @@
 ﻿<?php
 echo"<h1><center>Functions in PHP</center></h1>";
+echo"A function is a block of statement that can be used repeatedly  in a program.<br> A function will not execute automatically when a page loads.<br> A function will be executed by a call to the function.<br> A function may have zero , one or more parameters.<br> A function may return a value.";
+
+
+
 
 ?>
